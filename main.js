c04b40ab0bcb971e23ed3c788ffa2d6ed27673d6
Emanuel-P16 <palazon.emanuel@gmail.com>
2021-08-09 16:43:24 -0300

    se agrego la posibilidad de traer de la base de datos OMBD los datos de la pelicula

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
-APIKEY ="JbeNSOlKgmtXkourFWex "
-APIURL ="https://the-one-api.dev/v2/book"
-const headers ={
-    'Accept': 'application/json',
-    'Authorization': 'Bearer' + APIKEY
-}
+APIKEYLOTR ="JbeNSOlKgmtXkourFWex "
+APIURLLOTR ="https://the-one-api.dev/v2/movie"
+APIKEYOMDB ="bfe5262b"
+APIURLOMBD ="http://www.omdbapi.com/?t=The+Hobbit%3A+An+Unexpected+Journey&apikey=bfe5262b"
+//http://www.omdbapi.com/?t=The+Hobbit%3A+An+Unexpected+Journey
+//"http://www.omdbapi.com/?apikey=bfe5262b&?t=The+Hobbit%3A+An+Unexpected+Journey"
 
-const title = document.querySelector(".title")
-async function getBooks(){
-    const resp = await fetch(APIURL,{headers: headers});
+const movieContainer = document.querySelector(".movieContainer")
+const movieList = document.querySelector(".movieList")
+
+async function getMovies(){
+    const resp = await fetch(APIURLLOTR,{
+        headers:{ 
+    'Accept': 'application/json',
+    'Authorization': 'Bearer JbeNSOlKgmtXkourFWex ',    
+    }
+    });
     const respData = await resp.json();
     console.log(respData)
-    console.log(respData.docs[0].name)
-    title.innerHTML = respData.docs[0].name
+   /*for (let index = 0; index < respData.docs.length; index++) {
+       const element = respData.docs[index].name;
+       newLi = document.createElement("li")
+       newLi.className = "elementList"
+       newLi.innerText = element
+       movieList.appendChild(newLi) 
+       console.log(element)
+   }*/
+  
+   respData.docs.forEach((movie) => {
+    //console.log(movie)
+    const element = movie.name;
+    newLi = document.createElement("li")
+    newLi.className = "elementList"
+    newLi.innerText = element
+    movieList.appendChild(newLi) 
+   
+   });
+    //title.innerHTML = respData.docs[0].name
     return respData
 }
 
-getBooks()
+async function getImageMovie(){
+    const resp = await fetch(APIURLOMBD)
+    respData = await resp.json()
+    console.log(respData)
+    return respData
+}
+//getMovies()
+getImageMovie()
